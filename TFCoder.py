9ce58c0c30d7d49c5a18719b00e28ad8684ad967
Matt <usmcamp0811@gmail.com>
2017-01-04 21:33:00 -0600

    Update TFCoder.py
    Re-wrote most of this to clean it up.

--- TFCoder.py
+++ TFCoder.py
@@ -8,73 +8,78 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import PIL
+from PIL import Image, ImageOps
+from tqdm import tqdm
+import pandas as pd
+import os
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
 
 def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
+def _float_feature(value):
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-#takes a csv file in the format of: path,label
-def read_qfile(qfile):
-    q = pd.read_csv(qfile, header = None)
-    q.columns = ['path', 'label']
-    labels = q.label.tolist()
-    path = q.path.tolist()
-    return path, labels
+def make_q_list(filepathlist):
+    filepathlist = filepathlist
+    filepaths = []
+    labels = []
+    for path in filepathlist:
+        image_files = os.listdir(path)
+        for image in image_files:
+            if image.endswith('.jpg'):
+                image_file = os.path.join(path, image)
+                image_file = image_file
+                image_label = os.path.basename(os.path.normpath(path))
+                filepaths.append(image_file)
+                labels.append(image_label)
 
-'''takes the list of plane text labels (ie Open, Closed) and converts to 1 and 0.
-this will return an array of all the labels.
-Example:
-labels = ['open','open','open','closed','closed','open','open','closed']
-int_classes = [0,0,0,1,1,0,0,1]
-index = ['open', 'closed']'''
+    return filepaths, labels
+
 def label_to_int(labels=None, index=None): #labels is a list of labels
     class_index = index
     int_classes = []
     for label in labels:
-      int_classes.append(class_index.index(label)) #the class_index.index() looks values up in the list label
+        int_classes.append(class_index.index(label)) #the class_index.index() looks values up in the list label
     int_classes = np.array(int_classes, dtype=np.uint32)
     return int_classes
 
-#just a helper function to read in all the images to an array
-def read_images(pathlist=None):
+def read_images(pathlist, lbl_index_list, size=None):
     images = []
     labels = []
-    for file in pathlist:
+    for file in tqdm(pathlist):
         im = Image.open(file)
         im = np.asarray(im, np.uint8)
-        image_name = file.split('/')[-1].split('.')[0]
-        if "open" in image_name:
-            image_name = 1
-        elif "closed" in image_name:
-            image_name = 0
-        else:
-            image_name = 99
-        images.append([image_name,im])
+        if size != None:
+            im = image_resizer(im, size)
+        image_label_text = [os.path.basename(os.path.dirname((file)))]
+        image_label_int = label_to_int(labels=image_label_text, index=lbl_index_list)
+        images.append([image_label_int,im])
     images = sorted(images, key = lambda image: image[0])
+    image_shape = images[0][1].shape
     images_only = [np.asarray(image[1].flatten(), np.uint8) for image in images]
     images_only = np.array(images_only)
     labels_only = [np.asarray(key[0], np.int32) for key in images]
     labels_only = np.array(labels_only)
-    # print(images_only.shape)
-    # print(labels.shape)
-    return images_only, labels_only #flat images
 
+    return images_only, labels_only, image_shape #flat images
 
-def convert_to_TF(images, labels, name):
+def convert_to_TF(images, labels, image_shape, name):
     label_count = labels.shape[0]
     print('There are %d images in this dataset.' % (label_count))
     if images.shape[0] != label_count:
         raise ValueError('WTF! Devil! There are %d images and %d labels. Go fix yourself!' %
                          (images.shape[0], label_count))
-    # rows = images.shape[1]
-    # cols = images.shape[2]
-    # depth = images.shape[3]
-    rows = 100
-    cols = 100
-    depth = 3
+    rows = image_shape[0]
+    cols = image_shape[1]
+    depth = image_shape[2]
 
     filename = os.path.join(name + '.tfrecords')
     print('Writing', filename)
@@ -89,24 +94,27 @@ def convert_to_TF(images, labels, name):
             'image_raw': _bytes_feature(image_raw)}))
         writer.write(example.SerializeToString())
 
+def image_resizer(image,size):
+
+    basewidth = size
+    img = Image.fromarray(image)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+    #crop the sides off to make a square image
+    img = ImageOps.fit(img, (size,size), Image.ANTIALIAS)
+
+    return np.asarray(img)
+
+def label_on_image(image, image_label_text):
+    img = Image.fromarray(image)
+    draw = ImageDraw.Draw(img)
+    # font = ImageFont.load_default().font
+    font = ImageFont.truetype("arial.ttf", 45)
+    label = image_label_text
 
 
-path, labels = read_qfile('queue.txt')
-X_train, X_test, y_train, y_test = train_test_split(
-    path, labels, test_size=0.10, random_state=42)
+    draw.text((10, 10), str(image_label_text), (255, 0, 0), font=font)
+    # img = np.reshape(np.asarray(img), [160, 160, 3])
 
-label_index = ['open', 'closed']
-# y_train = label_to_int(y_train, label_index)
-# y_test = label_to_int(y_test, label_index)
-X_train, y_train = read_images(pathlist=X_train)
-X_test, y_test = read_images(pathlist=X_test)
-num = random.randint(0,8000)
-print(y_train[num])
-sample_im = np.reshape(X_train[num], [224, 224, 3])
-plt.imshow(np.asarray(sample_im, np.uint8))
-plt.show()
-convert_to_TF(X_train, y_train, 'garage_door224_TRAIN')
-convert_to_TF(X_test, y_test, 'garage_door224_TEST')
-
-train_size = (8243, 30000)
-
+    return np.asarray(img)
